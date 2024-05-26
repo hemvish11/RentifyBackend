bcf922be7ba5e3636f16a24a76495112cd18a59d
hemant <hemvish11@gmail.com>
2024-05-26 15:05:13 +0530

    rentify backend completed

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+//To take input as JSON in backend's request body
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected to Mongo");
+    })
+    .catch((err) => {
+        console.log("Error connecting to Mongo:", err);
+    });
+
+app.listen(3000, () => {
+    console.log("Server chalu h... jao dhoom machao...")
+})
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+
+// middleware to handle errors
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    });
+})
